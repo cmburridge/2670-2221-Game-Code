aea751532cd02d4a2c642b7f77386a27fa76ed98
Cameron burridge <cmburridge4022@gmail.com>
2020-10-05 15:39:37 -0600

    Started jump code

---
+++ Jump Double Jump/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float yVar;
+    public float jumpCount = 2f;
+    public float currentJumpCount;
+    public float gravity = 9.138f;
+    public float jumpForce = 10f;
+    public float rotateSpeed = 10f;
+    
+    public Vector3 movement;
+    public CharacterController controller;
+
+    void Update()
+    {
+        var hInput = Input.GetAxis("Horizontal")* Time.deltaTime*rotateSpeed;
+        transform.Rotate(0,hInput,0);
+        
+        yVar += gravity * Time.deltaTime;
+
+        if (controller.isGrounded && movement.y < 0)
+        {
+            yVar = -1f;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+
+        {
+            yVar = jumpForce;
+            jumpCount++;
+        }
+        
+        movement = transform.TransformDirection(movement); 
+        controller.Move(movement * Time.deltaTime);
+    }
+}
