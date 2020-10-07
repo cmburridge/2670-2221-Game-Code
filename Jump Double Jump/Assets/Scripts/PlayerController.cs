50afed8fa8e4e9f0a3e2d5605b56e71d22a79dc0
Cameron burridge <cmburridge4022@gmail.com>
2020-10-07 16:35:51 -0600

    Added full movement and fixed camera

--- Jump Double Jump/Assets/Scripts/PlayerController.cs
+++ Jump Double Jump/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float yVar;
-    public float jumpCount = 2f;
-    public float currentJumpCount;
-    public float gravity = 9.138f;
-    public float jumpForce = 10f;
-    public float rotateSpeed = 10f;
-    
-    public Vector3 movement;
-    public CharacterController controller;
+    private CharacterController controller;
+    private Vector3 movement;
 
-    void Update()
+    public float rotateSpeed = 5f, gravity = -9.81f, jumpForce = 10f;
+    private float yVar;
+
+    public FloatData moveSpeed, fastSpeed;
+    public FloatData normalSpeed;
+
+    public FloatData MoveSpeed => moveSpeed;
+
+    public IntData playerJumpCount;
+    private int jumpCount;
+    
+    private void Start()
     {
+        moveSpeed = normalSpeed;
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        {
+            var vInput = Input.GetAxis("Vertical")*moveSpeed.value;
+            movement.Set(vInput,yVar,0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed = fastSpeed;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = normalSpeed;
+        }
+        
         var hInput = Input.GetAxis("Horizontal")* Time.deltaTime*rotateSpeed;
         transform.Rotate(0,hInput,0);
-        
-        yVar += gravity * Time.deltaTime;
+
+        yVar += gravity*Time.deltaTime;
 
         if (controller.isGrounded && movement.y < 0)
         {
@@ -27,14 +52,14 @@ public class PlayerController : MonoBehaviour
             jumpCount = 0;
         }
 
-        if (Input.GetButtonDown("Jump"))
-
+        if (Input.GetButtonDown("Jump") && jumpCount< playerJumpCount.value)
         {
             yVar = jumpForce;
             jumpCount++;
         }
-        
-        movement = transform.TransformDirection(movement); 
+
+        movement = transform.TransformDirection(movement);
         controller.Move(movement * Time.deltaTime);
+        
     }
 }
