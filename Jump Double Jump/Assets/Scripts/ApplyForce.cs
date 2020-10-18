5a8db16c3b67b912491f74b9b11faaf0b1542b99
Cameron burridge <cmburridge4022@gmail.com>
2020-10-17 19:53:18 -0600

    Messed with bullet directions, still fires in wrong direction

--- Jump Double Jump/Assets/Scripts/ApplyForce.cs
+++ Jump Double Jump/Assets/Scripts/ApplyForce.cs
@@ -7,11 +7,14 @@ public class ApplyForce : MonoBehaviour
 {
    private Rigidbody rBody;
    public float force = 30f;
-
+   public Transform playerDirection;
    void Start()
    {
       rBody = GetComponent<Rigidbody>();
-      var forceDirection = new Vector3(force,0,0);
-      rBody.AddRelativeForce(forceDirection);
+   }
+
+   private void FixedUpdate()
+   {
+      rBody.AddForce(playerDirection.forward.normalized * force);
    }
 }
