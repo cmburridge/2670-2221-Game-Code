168b9949b6ce64c9dce320ae9b706f3b53ea0a6b
Cameron burridge <cmburridge4022@gmail.com>
2020-10-28 20:46:42 -0600

    Changed layout, added activating switch

---
+++ Labyrinth Lockdown/Assets/Scripts/ActivateBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateBehavior : MonoBehaviour
+{
+    public GameObject activatedObj;
+    private void OnTriggerEnter(Collider other)
+    {
+        activatedObj.SetActive(true);
+    }
+
+    private void Start()
+    {
+        activatedObj.SetActive(false);
+    }
+}
