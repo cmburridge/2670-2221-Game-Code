7fe747a7271443c958286b404ce8f11d4a9a93ec
Cameron burridge <cmburridge4022@gmail.com>
2020-10-30 21:17:44 -0600

    Edited levels and fixed switch and activater code

--- Labyrinth Lockdown/Assets/Scripts/ActivateBehavior.cs
+++ Labyrinth Lockdown/Assets/Scripts/ActivateBehavior.cs
@@ -6,13 +6,27 @@ using UnityEngine;
 public class ActivateBehavior : MonoBehaviour
 {
     public GameObject activatedObj;
+    public GameObject activaterObj;
+    public bool isActive = false;
     private void OnTriggerEnter(Collider other)
     {
-        activatedObj.SetActive(true);
+        if (isActive == false)
+        {
+            activatedObj.SetActive(true);
+            activaterObj.SetActive(false);
+            isActive = true;
+        }
+        else
+        {
+            activatedObj.SetActive(false);
+            activaterObj.SetActive(true);
+            isActive = false;
+        }
     }
 
     private void Start()
     {
         activatedObj.SetActive(false);
+        activaterObj.SetActive(true);
     }
 }
