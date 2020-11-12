1ca996efc5056dccfc886526b066143cbe0b141d
Cameron burridge <cmburridge4022@gmail.com>
2020-11-11 21:55:40 -0700

    Tried to code powerUp not succesful, messed with lighting

---
+++ Labyrinth Lockdown/Assets/Scripts/ValueEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueEvents : MonoBehaviour
+{
+   public FloatData objValue;
+   public float newValue;
+   
+   public void AddValue()
+   {
+      objValue.value += newValue;
+   }
+
+   public void SubtractValue()
+   {
+      objValue.value -= newValue;
+   }
+}
